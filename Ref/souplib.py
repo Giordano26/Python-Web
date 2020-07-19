fdf90755a36736c12e2d17ade789b1eb03374fe2
Stefano <smfgiordano@gmail.com>
2020-07-19 04:22:13 -0300

    urllib and beatifulsoup ex

---
+++ Ref/souplib.py
@@ -0,0 +1,10 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser') #cleans html stuff
+
+#Retrieve all of the anchor tags
+tags = soup('a') 
+for tag in tags:
+    print(tag.get('href', None)) #search for href
